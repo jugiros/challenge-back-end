687a25c5d158b2154e8f7aa6f3ffb7e5cb5ad8d9
jugiros <31550758+jugiros@users.noreply.github.com>
2023-07-22 20:50:12 -0500

    feat: Adding ApplicationDbContext

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using challenge_back_end.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace challenge_back_end.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Person> Person { get; set; }
+    }
+}
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,10 +1,11 @@
 ﻿namespace challenge_back_end.Models
 {
-    public class Persona
+    public class Person
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string? LastName { get; set; }
+        public string? Address { get; set; }
         public int Age { get; set; }
         public DateTime BirthDate { get; set; }
     }
